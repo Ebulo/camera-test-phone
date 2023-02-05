86b86cfde9e850c3ba394a998f953c83ee7cb7db
Bishant Nayak <bishant.nayak44@gmail.com>
2023-02-05 14:27:26 +0530

    Updated the name of the file sent and sent the dte additionally

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 function saveSnap(res, img_name) {
+    // let msg = document.getElementById("msg");
     let url = "https://script.google.com/macros/s/AKfycbzf3f7B7Pqo7nX67aBXhOrmvOTmYh8Hng4c6r0to_MokP2ZdpIbi40gxlZY2mwm6i5Z/exec";
     
     let spt = res.split("base64,")[1];
@@ -7,13 +8,18 @@ function saveSnap(res, img_name) {
     let obj = {
         base64:spt,
         type:"image/jpeg",
-        name: img_name + "_" + date + "_.jpg"
+        name: img_name + ".jpg",
+        date: date
     }
     fetch(url,{
         method:"POST",
         body:JSON.stringify(obj)
     })
     .then(r=>r.text())
-    .then(data=>console.log(data))
+    .then(data => {
+        console.log(data);
+        // msg.style.display = "block";
+        alert("Added Successfully")
+    })
 
 }
