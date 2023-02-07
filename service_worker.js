cb53b60f4f6b4120a062187dcc584702ececd706
Bishant Nayak <bishant.nayak44@gmail.com>
2023-02-07 12:15:09 +0530

    Updated the Service_worker to do not cache

--- service_worker.js
+++ service_worker.js
@@ -1,10 +1,11 @@
 // Cached core static resources 
 self.addEventListener("install",e=>{
-    e.waitUntil(
-      caches.open("static").then(cache=>{
-        return cache.addAll(['soa_logo_192.png','soa_logo_512.png','soa_logo.png']);
-      })
-    );
+    // e.waitUntil(
+    //   caches.open("static").then(cache=>{
+    //     return cache.add(['soa_logo_192.png','soa_logo_512.png','soa_logo.png']);
+    //   })
+    // );
+    console.log("Service Worker Added, App installed");
   });
   
   // Fatch resources
